1b6c0e37cba13ac9a345e2a9c82432630570570f
shimul <shimuluse@gmail.com>
2017-10-01 10:56:38 +0600

    Voyage practice: undescribed commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /* adding active class to nav link which is the current page */
 $(document).ready(function() {
     $("nav [href]").each(function() {
-        if (this.href == window.location.href) {
+        if (this.href === window.location.href) {
             $(this).addClass("active");
         }
     });
