a5e726e09ad8626ebfe34ce6910834c59340b1c9
shimul <shimuluse@gmail.com>
2017-10-01 10:35:09 +0600

    Voyage practice: adding jquery.min.js to the folder

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* adding active class to nav link which is the current page */
+$(document).ready(function() {
+    $("nav [href]").each(function() {
+        if (this.href == window.location.href) {
+            $(this).addClass("active");
+        }
+    });
+});
+/* ends */
